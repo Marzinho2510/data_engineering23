00fcce60e4ec8959218cbd6be2590bf70c74185f
Marzinho2510 <vietphu.bui95@gmail.com>
2023-11-01 14:58:34 +0100

    API source

---
+++ API_file_source.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import random
+from faker import Faker
+
+app = FastAPI()
+fake = Faker()
+
+@app.get("/generate_users")
+def generate_users(count: int = 1000):
+    users = []
+    for _ in range(count):
+        user = {
+            "name": fake.name(),
+            "age": random.randint(18, 65),
+            "location": fake.city()
+        }
+        users.append(user)
+    return users
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
