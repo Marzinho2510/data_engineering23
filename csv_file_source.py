7a13d60f0fc9ea6e5e4f486665caebadeea5087a
Marzinho2510 <vietphu.bui95@gmail.com>
2023-11-01 14:49:18 +0100

    question 1

---
+++ csv_file_source.py
@@ -0,0 +1,25 @@
+import csv
+import random
+from faker import Faker
+
+#create 10,000 records of random data
+data = []
+fake = Faker()
+for _ in range(10000):
+    user = {
+        'Name': fake.name(),
+        'Age': random.randint(0, 105),
+        'Location': fake.city(),
+    }
+    data.append(user)
+
+#save data into a csv file
+csv_file = 'random_data.csv'
+columns = ['Name', 'Age', 'Location']
+
+with open(csv_file, mode='w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=columns)
+    writer.writeheader()
+
+    for row in data:
+        writer.writerow(row)
